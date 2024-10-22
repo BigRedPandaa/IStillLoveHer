5e2a5d187a6a3073d247e9e7aebf38f16e3ed830
BigRedPandaa <wiznewski157@gmail.com>
2024-10-21 22:50:17 -0400

    RoomBuild

--- i-still-love-her/Scripts/notes.gd
+++ i-still-love-her/Scripts/notes.gd
@@ -5,10 +5,12 @@ extends Interactable
 
 @onready var notes_label: RichTextLabel = $CanvasLayer/Notes/BG/NotesLabel
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
-
+@onready var note_sound: AudioStreamPlayer3D = $NoteSound
 
 func _ready() -> void:
-	interacted.connect(func(_x): canvas_layer.visible = true)
+	interacted.connect(func(_x):
+		if !canvas_layer.visible: note_sound.play()
+		canvas_layer.visible = true)
 	notes_label.text = note
 	canvas_layer.visible = false
 
@@ -16,7 +18,8 @@ func _ready() -> void:
 func _input(_event: InputEvent) -> void:
 	if !canvas_layer.visible:
 		return
-	
+
+
 	if Input.is_action_just_pressed(&"ui_cancel"):
 		get_viewport().set_input_as_handled()
 		canvas_layer.visible = false
